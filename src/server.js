fc056c3fe5230de69d43238bcb0822593a743a75
Bora <boraaaydin@gmail.com>
2019-05-04 15:56:41 +0300

    2.7 Building the Controllers. Controllers added instead of routing in server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var controllers = require("./controllers");
+
 app.set("view engine", "vash");
 
-app.get("/", function (req, res) {
-    res.render("index", { title: "Express+vash" });
-});
-
+controllers.init(app);
 
 var server = http.createServer(app);
 server.listen(3000);
